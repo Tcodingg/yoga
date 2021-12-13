ed71267f24c45aa5588e42fe5b3227a1ed0df11c
Tcodingg <princetlive@yahoo.com>
2021-12-13 16:15:15 -0500

    add react-slick afterEach function and save the values in state

--- src/components/Slides/Slides.js
+++ src/components/Slides/Slides.js
@@ -1,9 +1,12 @@
+import { useEffect, useState } from "react";
 import Slider from "react-slick";
 import "slick-carousel/slick/slick.css";
 import "slick-carousel/slick/slick-theme.css";
 
 import "./Slides.css";
 const Slides = () => {
+   const [afterAll, setAfterAll] = useState(0);
+
    const settings = {
       dots: true,
       infinite: true,
@@ -15,7 +18,13 @@ const Slides = () => {
       arrows: true,
       className: "react-slick-parent",
       dotsClass: "dotsClass",
+      afterChange: (state) => setAfterAll(state),
    };
+   useEffect(() => {
+      console.log(afterAll);
+   }, [afterAll]);
+
+   console.log(afterAll);
    return (
       <Slider {...settings}>
          <div className="slide-wrapper">
