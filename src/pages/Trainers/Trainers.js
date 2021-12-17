312784f9b89609eb6eb817746e28c78c2c91f54c
Tcodingg <princetlive@yahoo.com>
2021-12-17 11:22:22 -0500

    add more content to trainers page and style them

--- src/pages/Trainers/Trainers.js
+++ src/pages/Trainers/Trainers.js
@@ -1,6 +1,33 @@
+import TopBanner from "../../components/banners/TopBanner";
+import TrainersSlide from "../../components/Trainers/TrainersSlide";
 import "./Trainers.css";
+import { trainersData } from "./trainersData";
 const Trainers = () => {
-   return <div>trainers</div>;
+   return (
+      <section className="trainers">
+         <TopBanner />
+         <div className="bd-container trainers-container">
+            <div className="section trainers-wrapper">
+               <div className="trainers-content">
+                  {trainersData.map(({ img, title, details }) => {
+                     return (
+                        <div className="trainers-content">
+                           <div className="right-trainers-content">
+                              <hi className="title">{title}</hi>
+                              <p className="text">{details}</p>
+                           </div>
+                           <div className="left-trainers-content">
+                              <img src={img} alt="" />
+                           </div>
+                        </div>
+                     );
+                  })}
+               </div>
+               <TrainersSlide />
+            </div>
+         </div>
+      </section>
+   );
 };
 
 export default Trainers;
---
+++ src/pages/Trainers/trainersData.js
@@ -0,0 +1,24 @@
+import training1 from "../../images/trainers1.png";
+import training2 from "../../images/trainers2.png";
+import training3 from "../../images/trainers3.png";
+
+export const trainersData = [
+   {
+      img: training2,
+      title: "professional trainers",
+      details:
+         "Lorem ipsum dolor sit amet consectetur adipisicing elit. Minima laborum totam expedita odio, doloremque, reprehenderit error suscipit nisi, nobis nostrum quia sint temporibus veritatis assumenda voluptate quidem! Libero, numquam! Illum, dolorem delectus. Obcaecati inventore voluptas impedit iusto quia tenetur! Voluptatem nam autem a numquam et quam, cum quos eveniet, repudiandae quae impedit perspiciatis magni iste, illo debitis incidunt ipsa enim.",
+   },
+   {
+      img: training1,
+      title: "Plan that suites you",
+      details:
+         "Lorem ipsum dolor sit amet consectetur adipisicing elit. Minima laborum totam expedita odio, doloremque, reprehenderit error suscipit nisi, nobis nostrum quia sint temporibus veritatis assumenda voluptate quidem! Libero, numquam! Illum, dolorem delectus. Obcaecati inventore voluptas impedit iusto quia tenetur! Voluptatem nam autem a numquam et quam, cum quos eveniet, repudiandae quae impedit perspiciatis magni iste, illo debitis incidunt ipsa enim.",
+   },
+   {
+      img: training3,
+      title: "focused training areas",
+      details:
+         "Lorem ipsum dolor sit amet consectetur adipisicing elit. Minima laborum totam expedita odio, doloremque, reprehenderit error suscipit nisi, nobis nostrum quia sint temporibus veritatis assumenda voluptate quidem! Libero, numquam! Illum, dolorem delectus. Obcaecati inventore voluptas impedit iusto quia tenetur! Voluptatem nam autem a numquam et quam, cum quos eveniet, repudiandae quae impedit perspiciatis magni iste, illo debitis incidunt ipsa enim.",
+   },
+];
